f8156469fe3781ead7d3ee56ee6450a63cd13659
Alan404 <nguyentri.alan@gmail.com>
2023-12-04 11:26:57 +0700

    update

--- create_dataset.py
+++ create_dataset.py
@@ -4,7 +4,9 @@ import pandas as pd
 from glob import glob
 from tqdm import tqdm
 
-os.mkdir("./datasets")
+if os.path.exists("./datasets") == False:
+    os.mkdir("./datasets")
+
 os.system('curl -L -o ./datasets/infore_technology.zip "https://huggingface.co/datasets/ntt123/infore/resolve/main/infore_16k_denoised.zip"')
 os.system('unzip ./datasets/infore_technology.zip -d ./datasets/infore_technology')
 
@@ -12,6 +14,7 @@ audio_paths = glob("./datasets/infore_technology/data/*.wav")
 
 labels = []
 
+print("Creating ManiFest Dataset...")
 for path in tqdm(audio_paths):
     text_path = path.replace(".wav", ".txt")
     text = io.open(text_path).read().strip().split("\n")
@@ -23,3 +26,5 @@ pd.DataFrame({
     'path': audio_paths,
     'text': labels
 }).to_csv("./datasets/data.csv", sep="\t", index=None)
+
+print(f"Dataset is saved at ./datasets")
